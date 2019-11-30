de58daeddbc1a10c9ada3910f203a139c70b82d9
manuelberrueta <manuelberrueta@gmail.com>
2019-11-30 15:01:44 -0800

    Initial commit

---
+++ pagerankestimator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i=0;
+    
+    while(i < 3){
+        puts("\t-=TESTING, YAY IT COMPILES!\n\n");
+        i++;
+    }
+    return 0;
+}
