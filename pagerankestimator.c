878f5c79f263e9170d254b22d8226a368f85601c
Berrueta, Manuel <manuel.berrueta@gmail.com>
2019-12-01 15:16:19 -0800

    Good start code for now

--- pagerankestimator.c
+++ pagerankestimator.c
@@ -14,7 +14,8 @@ int main(int argc, char const *argv[])
         getchar();
         return -1;
     }
-
+    int i=0;
+    int j=0;
     int K = atoi(argv[1]);
     //double D = atof(argv[2]);
     double D = -1.0;
@@ -24,6 +25,18 @@ int main(int argc, char const *argv[])
     int t = atoi(argv[4]);
     int nSize = atoi(argv[5]);
     DATA myGraph[nSize];
+    while(i < nSize){
+        myGraph[i].pagerank = 0;
+        while(j < 64){
+            myGraph[i].hyperlinks[j] = 0;
+            j++;
+        }
+        i++;
+    }
+
+
+
+
     
     if(DEBUG){
         printf("K=%d  |  D=%lf  |  fileName=%s  |  t=%d  |  nSize=%d\n", K, D, fileName, t, nSize);
@@ -38,9 +51,9 @@ int main(int argc, char const *argv[])
     char buf[512] = {0};
     int node = -1;
     int hyperlink = -1;
-    int i=0;
-    int j=0;
-    while(j < 12){//feof(fp)){
+    i=0;
+    j=0;
+    while(j < 24){//feof(fp)){
         fgets(buf, 512, fp);
 
         if(buf[0] == '#'){
@@ -55,7 +68,8 @@ int main(int argc, char const *argv[])
             i=0;
             sscanf(buf, "%d %d", &node, &hyperlink);
             printf("node=%d  |  hyperlink=%d\n", node, hyperlink);
-            while(myGraph[j].hyperlinks[i]){
+            //iterate to find the next empty array spot to add a hyperlink to
+            while(myGraph[node].hyperlinks[i]){
                i++; 
             }
             myGraph[node].hyperlinks[i] = hyperlink;//TODO: Add the end
@@ -63,15 +77,22 @@ int main(int argc, char const *argv[])
             //TODO: use random on the arraysize-1 to choose an index randomly to jump to
         }
         //* if next line is last line then keep node as size
+        if(DEBUG){
+            if(j == 17){
+                j++;
+                j--;
+            }
+        }
         j++;
     }
 
+    //Print out contents of the first 18 links of the graph
     if(DEBUG){
         i=0, j=0;
-        while(myGraph[i].hyperlinks != 0){
+        while(i < 24){
             j=0;
-            while(myGraph[i].hyperlinks[j] = 0){
-                printf("myGraph[%d].hyperlinks[%d] = %d", i, j, (myGraph[i].hyperlinks[j]));
+            while(myGraph[i].hyperlinks[j] != 0){
+                printf("myGraph[%d].hyperlinks[%d] = %d\n", i, j, (myGraph[i].hyperlinks[j]));
                 j++;
             }
             j=0;
