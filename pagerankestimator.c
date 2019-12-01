d66ec1eeb87cce9194fcd1cde005327407e48ad5
manuelberrueta <manuelberrueta@gmail.com>
2019-12-01 08:29:27 -0800

    Updated starting code

--- pagerankestimator.c
+++ pagerankestimator.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <omp.h>
+
+//TODO: To compile: gcc -o <execname> -fopenmp <source file names>
+
+//*** Inputs listed in sequence
+//* K:<length of random walk>, D:<damping ratio>, input:<input file>, t:<# of threads>
+int DEBUG = 1;
 
 int main(int argc, char const *argv[])
 {
-    int i=0;
-    
-    while(i < 3){
-        puts("\t-=TESTING, YAY IT COMPILES!\n\n");
-        i++;
+    if(argc < 5){
+        puts("Please enter the required input:");
+        puts("K:<length of random walk>, D:<damping ratio>, input:<input file>, t:<# of threads>");
+        getchar();
+        return -1;
     }
+
+    int K = atoi(argv[1]);
+    float D = atof(argv[2]);
+    char fileName[64] = {0};
+    strcpy(fileName, argv[3]);
+    int t = atoi(argv[4]);
+    
+    if(DEBUG)
+    {
+        printf("K=%d  |  D=%f  |  fileName=%s  |  t=%d\n", K, D, fileName, t);
+    }
+
+    omp_set_num_threads(t);
+    
     return 0;
 }
